1a12b43fcd3a69ede67c447dc93d81505f3844ae
Nick Andryshak <nandryshak@gmail.com>
2024-01-04 16:28:59 -0500

    Initial commit

---
+++ doom_depths.py
@@ -0,0 +1,7 @@
+from doom_depths.equipment.items import this_function_is_in_items_py
+
+print("Running", __file__)
+
+this_function_is_in_items_py()
+
+print("Done")
---
+++ doom_depths/equipment/items.py
@@ -0,0 +1,2 @@
+def this_function_is_in_items_py():
+    print("Hello from", __file__)
